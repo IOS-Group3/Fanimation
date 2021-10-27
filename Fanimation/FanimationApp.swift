ab095e82a966ec1924b86888a4860cf9fbd5c199
samo7a <aelshetany@knights.ucf.edu>
2021-10-27 17:10:25 -0400

    Initial Commit

---
+++ Fanimation/FanimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FanimationApp.swift
+//  Fanimation
+//
+//  Created by Ahmed  Elshetany  on 10/27/21.
+//
+
+import SwiftUI
+
+@main
+struct FanimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
