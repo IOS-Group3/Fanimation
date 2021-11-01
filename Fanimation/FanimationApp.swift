50266a9edd8302a708c4016b0c52b9c88fe87b58
samo7a <aelshetany@knights.ucf.edu>
2021-10-31 21:18:14 -0400

    starting design the splash screen

--- Fanimation/FanimationApp.swift
+++ Fanimation/FanimationApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct FanimationApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+			LaunchScreen()
         }
     }
 }
