c7c41749a9334fd4071687530245bf64534be714
samo7a <aelshetany@knights.ucf.edu>
2021-11-13 16:12:40 -0500

    done with tab navigation styling

--- Fanimation/Screens/MainScreen.swift
+++ Fanimation/Screens/MainScreen.swift
@@ -8,12 +8,61 @@
 import SwiftUI
 
 struct MainScreen: View {
+	init() {
+		UITabBar.appearance().unselectedItemTintColor = UIColor.init(named: "light")
+		UITabBar.appearance().backgroundColor = .gray.withAlphaComponent(0.2)
+	}
+
+	@State var selectedTabIndex = 0
+	
+	private let tabBarImageNames = ["house.fill", "square.grid.2x2", "person.fill"]
+	private let tabBarText = ["Home", "My List", "Profile"]
 	var body: some View {
-		TabView{
-			HomeScreen().tabItem{Text("Home")}
-			MyListScreen().tabItem{Text("My List")}
-			ProfileScreen().tabItem{Text("Profile")}
+		GeometryReader { proxy in
+			ZStack {
+				VStack {
+					switch selectedTabIndex {
+						case 0:
+							HomeScreen()
+						case 1:
+							MyListScreen()
+						default:
+							ProfileScreen()
+					}
+					Spacer()
+					Divider()
+						.frame(height: 3.0)
+						.accentColor(.gray)
+					HStack {
+						ForEach(0..<tabBarImageNames.count, id: \.self) { num in
+							HStack {
+								Button(action: {
+									self.selectedTabIndex = num
+								}, label: {
+									Spacer()
+									VStack {
+										Image(systemName: tabBarImageNames[num])
+											.foregroundColor(selectedTabIndex == num ? .init("blue1") : .init("blue3"))
+											.frame(width: 50, height: 50)
+											.background(selectedTabIndex == num ? Color(red: 156/255, green: 197/255, blue:250/255) : Color.white.opacity(1))
+											.cornerRadius(20)
+											 
+										Text(tabBarText[num])
+											.foregroundColor(selectedTabIndex == num ? .init("blue1") : .init("blue3"))
+									}
+									
+									
+									Spacer()
+								})
+							}.font(.system(size: 17, weight: .semibold))
+						}
+					}
+					.padding(.bottom, proxy.safeAreaInsets.bottom)
+				}.edgesIgnoringSafeArea(.bottom)
+			}
+			
 		}
+
 	}
 }
 
